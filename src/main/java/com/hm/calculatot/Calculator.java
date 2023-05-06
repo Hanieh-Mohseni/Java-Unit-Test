abaa887a3ce78bb60a565670ac1fc68809308465
Hanieh <mohseni.hanieh@gmail.com>
2023-05-05 23:57:43 -0400

    Add ArrayTest

--- src/main/java/com/hm/calculatot/Calculator.java
+++ src/main/java/com/hm/calculatot/Calculator.java
@@ -1,4 +1,20 @@
 package com.hm.calculatot;
 
 public class Calculator {
-}
+    public static void main(String[] args) {
+
+        }
+
+    public int[]incrementArray(int[] values) {
+        int[]newValues = new int[values.length];
+        for (int i = 0; i < values.length; i++) {
+            newValues[i] = values[i]+1;
+
+        }
+
+        return newValues;
+
+    }
+
+    }
+
